a8473955d256065bc0482ae5e5e0d945ab82e4a7
LadyDivinee <ladydivineortega14@gmail.com>
2026-03-08 19:53:57 -0700

    checking

--- print_bigger_number.py
+++ print_bigger_number.py
@@ -3,3 +3,7 @@ num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
 #checking what number is bigger
 if num1 > num2:
+#checking
+    print("First number is bigger.")
+else:
+    print("Second number is bigger.")
