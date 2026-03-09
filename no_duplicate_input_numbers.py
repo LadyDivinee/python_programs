7e4e290c3e99dcb8b00087f5f7fd7ebb74c46025
LadyDivinee <ladydivineortega14@gmail.com>
2026-03-09 10:58:48 -0700

    fixed errors

--- no_duplicate_input_numbers.py
+++ no_duplicate_input_numbers.py
@@ -2,7 +2,7 @@
 no_duplicate_numbers = set()
 #for loop
 for i in range (10):
-    numbers = float(input(f"Enter number {i + 1}"))
+    numbers = float(input(f"Enter number {i + 1}: "))
 #.add to add the numbers in the set
     no_duplicate_numbers.add(numbers)
 print(no_duplicate_numbers)
