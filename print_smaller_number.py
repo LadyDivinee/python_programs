b3c72220bcae2164aff431dfdff39da2620de263
LadyDivinee <ladydivineortega14@gmail.com>
2026-03-08 23:56:54 -0700

    check if first number is smaller

--- print_smaller_number.py
+++ print_smaller_number.py
@@ -1,3 +1,6 @@
 #input number
 num1 = float(input("Enter the first number:"))
 num2 = float(input("Enter the second number:"))
+#check if num1 is smaller
+if num1 < num2:
+    print("First number is smaller.")
