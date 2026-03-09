0e118b8e7499f8f6872f632c9da793cc5b05d7a2
LadyDivinee <ladydivineortega14@gmail.com>
2026-03-09 00:15:48 -0700

    fixed my operator

--- print_if_not_equal.py
+++ print_if_not_equal.py
@@ -2,7 +2,7 @@
 num1 = float(input("Enter first number:"))
 num2 = float(input("Enter second number:"))
 #print "not equal" if not equal
-if num1 == num2:
+if num1 != num2:
     print("Not equal")
 else:
     print("Equal")
