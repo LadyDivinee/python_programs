09afb2b4a94869b128e25f3edb56d356a9c08b8d
LadyDivinee <ladydivineortega14@gmail.com>
2026-03-08 22:33:09 -0700

    fixed my error

--- sum_of_ten_numbers.py
+++ sum_of_ten_numbers.py
@@ -1,6 +1,8 @@
 total = 0
 for i in range(10):
 #input
-    numbers = float(input(f"Enter the number {i + 1}))
+    numbers = float(input(f"Enter the number{i+1}"))
 #for the total of numbers
     total += numbers
+#print the sum of 10 numbers
+print(f"The sum of 10 numbers is {total}")
