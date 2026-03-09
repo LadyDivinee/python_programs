0352710ea2b43582357f34ef03a0fd713e2e6472
LadyDivinee <ladydivineortega14@gmail.com>
2026-03-09 01:24:39 -0700

    to show the numbers between two numbers

--- numbers_between_two_numbers.py
+++ numbers_between_two_numbers.py
@@ -1,3 +1,7 @@
 #input numbers
 num1 = int(input("Enter first number:"))
 num2 = int(input("Enter second number:"))
+#to show the numbers between two numbers
+for i in range(num2 - num1 -1):
+    num1 += 1
+    print(num1)
