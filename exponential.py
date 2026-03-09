518b420dd599e54b282911013f308df29a8259ed
LadyDivinee <ladydivineortega14@gmail.com>
2026-03-08 21:27:50 -0700

    print the product when first number is raised to the second number

--- exponential.py
+++ exponential.py
@@ -1,3 +1,5 @@
 #input numbers
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
+#print the product when first number is raised to the second number
+print(num1 ** num2)
