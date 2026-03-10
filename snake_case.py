40c87b3c14e48383f8c443ad2ea045bce2f38c5d
LadyDivinee <ladydivineortega14@gmail.com>
2026-03-09 23:48:22 -0700

    formatting to snake case

--- snake_case.py
+++ snake_case.py
@@ -1,2 +1,5 @@
 #input full name in incorrect casing
 full_name = input("Enter full name in incorrect casing: ")
+#formatting to snake case
+snake_case = "_".join(full_name.lower().split())
+print(snake_case)
