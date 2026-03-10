622db383c7bdc5dcd68875208443b7e30aa566fc
LadyDivinee <ladydivineortega14@gmail.com>
2026-03-09 23:40:20 -0700

    format in pascal casing

--- pascal_case.py
+++ pascal_case.py
@@ -1,2 +1,5 @@
 #enter full name in incorrect casing
 full_name = input("Enter full name in incorrect casing:")
+#format in pascal casing
+pascal_case = "".join(full_name.title().split())
+print(pascal_case)
