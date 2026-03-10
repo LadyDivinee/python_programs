b8defd0dab5f1ca46f719d70af6730482d0deeb4
LadyDivinee <ladydivineortega14@gmail.com>
2026-03-09 23:07:10 -0700

    cleaned version

--- full_name_without_spaces.py
+++ full_name_without_spaces.py
@@ -1,2 +1,5 @@
 #enter full name with spaces at the beginning
 full_name = input("Enter your full name with spaces at the beginning: ")
+#cleaned version
+clean_name = full_name.lstrip()
+print(clean_name)
