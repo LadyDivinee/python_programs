c441143097d3673a7544d644009ce6ff6f17481a
LadyDivinee <ladydivineortega14@gmail.com>
2026-03-13 00:20:02 -0700

    print the output

--- not_upper().py
+++ not_upper().py
@@ -6,4 +6,5 @@ upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 translation_table = str.maketrans(lower_case, upper_case)
 #capitalizing letters
 name_in_capitalized = name.translate(translation_table)
-
+#print the output
+print(name_in_capitalized)
