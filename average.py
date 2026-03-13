869dbf55f27579a5a3415dad3246e944f7449a1c
LadyDivinee <ladydivineortega14@gmail.com>
2026-03-12 23:42:05 -0700

    while True loop

--- average.py
+++ average.py
@@ -1,2 +1,9 @@
 #input numbers list
 input_numbers = []
+#while True loop
+while True:
+    number = input("Enter a number: ")
+    try:
+        numbers = int(number)
+        input_numbers.append(numbers)
+        
