39fcd1a2f3bb817c5b21ac6b9e967c13d50ef542
LadyDivinee <ladydivineortega14@gmail.com>
2026-03-12 22:48:15 -0700

    while True loop

--- sorted_numbers.py
+++ sorted_numbers.py
@@ -1,2 +1,9 @@
 #input numbers
 input_numbers = []
+#while True loop
+while True:
+    numbers = input("Enter a number: ")
+    try:
+        numbers = int(numbers)
+        input_numbers.append(numbers)
+    
