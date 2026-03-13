e9d3db3694160ea949e037846b93c892efa12f2d
LadyDivinee <ladydivineortega14@gmail.com>
2026-03-12 23:26:11 -0700

    ValueError

--- descending_order.py
+++ descending_order.py
@@ -6,3 +6,7 @@ while True:
     try:
         numbers = int(numbers)
         input_numbers.append(numbers)
+#ValueError
+    except ValueError:
+        print("Only integers are allowed.")
+        break
