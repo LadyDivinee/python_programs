25db646be6ec819e7906eb6d428cd59807a56c6d
LadyDivinee <ladydivineortega14@gmail.com>
2026-03-12 22:29:21 -0700

    while True loop

--- unique_or_duplicate.py
+++ unique_or_duplicate.py
@@ -1,3 +1,14 @@
 #input numbers
 input_numbers = []
+#while True loop
+while True:
+    numbers = input("Enter number: ")
 
+    try:
+        numbers = int(numbers)
+
+        if numbers in input_numbers:
+            print ("Duplicate")
+        else:
+            print("Unique")
+        input_numbers.append(numbers)
