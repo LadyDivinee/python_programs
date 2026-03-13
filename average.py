0b8b979bb026db1bcf47f0861c95526ce72c9aa7
LadyDivinee <ladydivineortega14@gmail.com>
2026-03-12 23:44:10 -0700

    ValueError

--- average.py
+++ average.py
@@ -6,4 +6,7 @@ while True:
     try:
         numbers = int(number)
         input_numbers.append(numbers)
-        
+#ValueError
+    except ValueError:
+        print("Only integers are allowed.")
+        break
