31ca8765c1edd609f3d01085bad0d7c2ba6aa91f
LadyDivinee <ladydivineortega14@gmail.com>
2026-03-12 22:53:00 -0700

    fixed indentation and print the sorted numbers

--- sorted_numbers.py
+++ sorted_numbers.py
@@ -8,6 +8,8 @@ while True:
         input_numbers.append(numbers)
 #ValueError for decimal and not a number
     except ValueError:
-    print("Only integers are allowed.")
-    break
-#
+        print("Only integers are allowed.")
+        break
+#print in ascending order
+if numbers:
+    print("The numbers in ascending order are", sorted(input_numbers))
