018607c2c413cdb282d880696f358f0f5a3e8a31
LadyDivinee <ladydivineortega14@gmail.com>
2026-03-13 00:30:34 -0700

    check if in lower case

--- not_islower().py
+++ not_islower().py
@@ -3,3 +3,9 @@ name = "dibayn"
 #lower case and upper case string
 lower_case = "abcdefghijklmnopqrstuvwxyz"
 upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+#check if in lower case
+lower = True
+for ch in name:
+    if ch in lower_case:
+        lower = False
+        break
