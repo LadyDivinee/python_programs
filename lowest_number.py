193f72468d45f8d7f7eec9aa6730a2263be53b8e
LadyDivinee <ladydivineortega14@gmail.com>
2026-03-12 22:41:59 -0700

    ValueError for decimal or not a number

--- lowest_number.py
+++ lowest_number.py
@@ -6,3 +6,7 @@ while True:
     try:
         numbers = int(numbers)
         input_numbers.append(numbers)
+#ValueError for decimal or not a number
+    except ValueError:
+        print("Only integers are allowed.")
+        break
