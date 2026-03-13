b5ad071111d76524d3bb932f0b317e1470c0ae7e
LadyDivinee <ladydivineortega14@gmail.com>
2026-03-13 00:33:43 -0700

    fixed errors and to print the output

--- not_islower().py
+++ not_islower().py
@@ -2,10 +2,14 @@
 name = "dibayn"
 #lower case and upper case string
 lower_case = "abcdefghijklmnopqrstuvwxyz"
-upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 #check if in lower case
 lower = True
 for ch in name:
-    if ch in lower_case:
+    if ch not in lower_case:
         lower = False
         break
+#print output
+if lower:
+    print("All in lower case.")
+else:
+    print("Not all in lower case.")
