c1fd0ef2da8b2ac989164279873c309ab6eb9ab3
LadyDivinee <ladydivineortega14@gmail.com>
2026-03-13 00:01:21 -0700

    check the suffix

--- removesuffix().py
+++ removesuffix().py
@@ -3,3 +3,9 @@ name = "dibayn.mngs"
 suffix = ".mngs"
 #index of the suffix
 index = len(name) - len(suffix)
+#check the suffix
+if name [index:] == suffix:
+    just_name = name[:index]
+else:
+    just_name = name
+    
