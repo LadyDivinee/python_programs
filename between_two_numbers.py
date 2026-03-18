4b02636b90720b7e1d5b195fd156fda1247d95f6
LadyDivinee <ladydivineortega14@gmail.com>
2026-03-18 00:14:59 -0700

    check the range of the numbers

--- between_two_numbers.py
+++ between_two_numbers.py
@@ -1,4 +1,6 @@
 #input two numbers by user
 num_1 = int(input("Enter first number: "))
 num_2 = int(input("Enter second number: "))
-
+#check the range of the numbers
+smaller = min(num_1, num_2)
+larger = max(num_1, num_2)
