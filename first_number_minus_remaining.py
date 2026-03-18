14b986c98b897daf82daaa1dff32b9f11aafca56
LadyDivinee <ladydivineortega14@gmail.com>
2026-03-17 23:54:46 -0700

    to print the result

--- first_number_minus_remaining.py
+++ first_number_minus_remaining.py
@@ -3,3 +3,8 @@ numbers = []
 for i in range(10):
     input_numbers = float(input(f"Enter number {i + 1}:"))
     numbers.append(input_numbers)
+#to print the result
+result = numbers[0]
+for i in range (1,10):
+    result -= numbers[i]
+print(result)
