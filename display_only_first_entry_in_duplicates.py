b8da1dbba252beb3ab218f3a0ee570958271b1e0
LadyDivinee <ladydivineortega14@gmail.com>
2026-03-18 23:21:20 -0700

    check for duplicates

--- display_only_first_entry_in_duplicates.py
+++ display_only_first_entry_in_duplicates.py
@@ -3,3 +3,10 @@ numbers = []
 for i in range (10):
     num = float(input(f"Enter number {i + 1}: "))
     numbers.append(num)
+#check for duplicates
+duplicates = set()
+for num in numbers:
+    if num not in duplicates:
+        print(num)
+        duplicates.add(num)
+
