0c314b03176b2480fcc95f0f6387bf8528fae3c4
LadyDivinee <ladydivineortega14@gmail.com>
2026-03-19 20:34:57 -0700

    translation table

--- without_using_.lower().py
+++ without_using_.lower().py
@@ -1,2 +1,6 @@
 #string
 word = "DIBAYN MAANGAS"
+#translation table
+lowercase = "abcdefghijklmnopqrstuvwxyz"
+uppercase = "abcdefghijklmnopqrstuv"
+translation_table = str.maketrans(uppercase, lowercase)
