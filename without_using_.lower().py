7b5b9fe713bbc2e2aa8166b1aa1e361dbb722b17
LadyDivinee <ladydivineortega14@gmail.com>
2026-03-19 20:38:38 -0700

    fixed the translation table and print in lowercase

--- without_using_.lower().py
+++ without_using_.lower().py
@@ -2,5 +2,8 @@
 word = "DIBAYN MAANGAS"
 #translation table
 lowercase = "abcdefghijklmnopqrstuvwxyz"
-uppercase = "abcdefghijklmnopqrstuv"
+uppercase = "abcdefghijklmnopqrstuvwxyz"
 translation_table = str.maketrans(uppercase, lowercase)
+#print in lowercasing
+word_in_lowercase = word.translate(translation_table)
+print(word_in_lowercase)
