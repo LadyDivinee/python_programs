c34957ca66dc1909d22b4a57de4c9b8bb67a7b14
LadyDivinee <ladydivineortega14@gmail.com>
2026-03-19 20:05:49 -0700

    to remove the spaces at the beginning

--- same_as_lstrip.py
+++ same_as_lstrip.py
@@ -3,4 +3,12 @@ text = input("Enter text with spaces at the beginning: ")
 
 result = ""
 spaces = False
+#to remove the spaces at the beginning
+for ch in text:
+    if ch != " ":
+        spaces = True
+    if spaces:
+        result += ch
+
+print (result)
 
