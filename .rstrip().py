dffd11727a5cff8ea0097875593b8ecfa92d9933
Ortega, Lady Divine D. <ladydivineortega14@gmail.com>
2026-03-19 19:24:32 -0700

    Delete .rstrip().py

--- .rstrip().py
+++
@@ -1,9 +0,0 @@
-#fixed string with spaces
-name = "dibayn.mngs    "
-#check the index of the space to remove spaces
-if name[-1] == " ":
-    just_name = name[:-1]
-else:
-    just_name = name
-#print the output of the fixed string
-print(just_name)
