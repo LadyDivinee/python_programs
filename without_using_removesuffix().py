7ab4181ef31df089c8faa46d7560ae7ac3ad994f
LadyDivinee <ladydivineortega14@gmail.com>
2026-03-20 00:59:51 -0700

    remove suffix and print output

--- without_using_removesuffix().py
+++ without_using_removesuffix().py
@@ -1,4 +1,10 @@
 #word and suffix
 word = "helpful"
 suffix = "ful"
+#remove suffix and print output
+if word[-len(suffix):] == suffix:
+    result = word[:-len(suffix)]
+else:
+    result = word
 
+print(result)
