3f5a6b0c9238b600b03a4c5afeabdf317f6af079
LadyDivinee <ladydivineortega14@gmail.com>
2026-03-19 20:59:36 -0700

    check if all in uppercase

--- without_using_.isupper().py
+++ without_using_.isupper().py
@@ -3,4 +3,11 @@ name = "DIBAYN MAANGAS"
 
 uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 upper = True
-
+#check if all in uppercase
+for ch in name:
+    if ch.isalpha() and ch not in uppercase:
+        upper = False
+if upper:
+    print("All in uppercase.")
+else:
+    print("Not all in uppercase.")
