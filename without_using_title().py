560e7ac9799ca541cd1bcc75b5264d1133a5ebf5
LadyDivinee <ladydivineortega14@gmail.com>
2026-03-19 22:25:01 -0700

    convert to title case

--- without_using_title().py
+++ without_using_title().py
@@ -3,4 +3,10 @@ name = "DIBAYN MAANGAS"
 #split text
 text = name.split()
 result = ""
+#convert to title case
+for word in text:
+    result += word[0].upper() + word[1:].lower() + " "
 
+result = result.rstrip()
+
+print(result)
