9cc1187bf6a720bf51173192a03aec9babe7f5b5
LadyDivinee <ladydivineortega14@gmail.com>
2026-03-19 21:56:41 -0700

    swapcase

--- without_using_swapcase().py
+++ without_using_swapcase().py
@@ -1,4 +1,11 @@
 #string
 word = "Dibayn Maangas"
 result = ""
+#swapcase
+for ch in word:
+    if ch == ch.lower():
+        result += ch.upper()
+    else:
+        result += ch.lower()
 
+print(result)
