b3410c72be07fa280e3e074144000d377a64e3a6
LadyDivinee <ladydivineortega14@gmail.com>
2026-03-19 20:29:15 -0700

    check the prefix and print the result

--- without_using_removeprefix().py
+++ without_using_removeprefix().py
@@ -1,4 +1,10 @@
 #string
 word = "disagree"
 prefix = "dis"
+#check the prefix and print the result
+if word[:len(prefix)] == prefix:
+    result = word[len(prefix):]
+else:
+    result = word
 
+print(result)
