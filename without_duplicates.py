364bb3ff6af2da822f912702e0d724451f513918
LadyDivinee <ladydivineortega14@gmail.com>
2026-03-19 23:28:53 -0700

    check the unique numbers and print output

--- without_duplicates.py
+++ without_duplicates.py
@@ -4,4 +4,8 @@ numbers = []
 for i in range(10):
     input_number = float(input(f"Enter number {i + 1}: "))
     numbers.append(input_number)
-
+#check the unique numbers and print output
+unique_numbers = set(numbers)
+for input_numbers in unique_numbers:
+    if numbers.count(input_numbers) == 1:
+        print(input_numbers)
